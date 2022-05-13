1b6c63a0d46b8dfa324fea80ca2ffa165a9bdcbe
Alrappie <alravimahesa@student.ub.ac.id>
2022-05-13 13:48:19 +0700

    a

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"PemWeb_BE/Database"
 	"PemWeb_BE/User"
 	"fmt"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,7 +15,19 @@ func main() {
 	fmt.Println("Database terinisialisasi")
 
 	r = gin.Default()
-	r.Use(cors.Default())
+	r.Use(func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    })
 
 	//router disini
 	User.Routes(db, r)
